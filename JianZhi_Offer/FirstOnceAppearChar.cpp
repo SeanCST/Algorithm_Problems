d852b7e7ece7536f884468000f61b3ac4157893f
SeanCST <seancst.x@gmail.com>
2018-08-17 22:37:34 +0800

    FirstOnceAppearChar

---
+++ JianZhi_Offer/FirstOnceAppearChar.cpp
@@ -0,0 +1,19 @@
+/* 剑指 Offer - 第一个只出现一次的字符
+
+ * 在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,
+ * 并返回它的位置, 如果没有则返回 -1（需要区分大小写）.
+ */
+ 
+class Solution {
+public:
+    int FirstNotRepeatingChar(string str) {
+        map <char, int> kMap;
+        for(int i = 0; i < str.length(); i++) 
+            kMap[str[i]] ++;
+        for(int i = 0; i < str.length(); i++)
+            if(kMap[str[i]] == 1)
+                return i;
+        
+        return -1;
+    }
+};
