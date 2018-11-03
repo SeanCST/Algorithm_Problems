78434dd0f661cf332f192e9ada134211251fc806
SeanCST <seancst.x@gmail.com>
2018-11-03 20:17:59 +0800

    1001_3n+1

---
+++ PAT_Basic/1001_3n+1.cpp
@@ -0,0 +1,22 @@
+/* https://pintia.cn/problem-sets/994805260223102976/problems/994805325918486528 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  // 输入
+  int n;
+  cin >> n;
+  
+  int count = 0;
+  while(n != 1) {
+    if(n % 2 == 0) // 偶数
+      n /= 2;
+    else // 奇数
+      n = (3 * n + 1) / 2 ;
+      
+    count ++;
+  }
+  
+  cout << count;
+}
