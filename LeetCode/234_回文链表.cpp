21a4da189e84d1e72615c35985f53686c92e66c7
SeanCST <seancst.x@gmail.com>
2019-02-22 19:53:42 +0800

    232、234

---
+++ LeetCode/234_回文链表.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next == NULL) return true;
+        if(head->next->next == NULL) {
+            if(head->val == head->next->val)
+                return true;
+            else
+                return false;
+        }
+        
+        // 使用快慢两个指针找到中点
+        ListNode *slow = head->next, *fast = head->next->next;
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // 旋转后半部分结点
+        ListNode *reverse = slow, *last = NULL;
+        while(reverse != NULL) {
+            ListNode *temp = reverse->next;
+            reverse->next = last;
+            last = reverse;
+            reverse = temp;
+        }
+        
+        // 比较前半部分和后半部分是否为回文
+        while(last != NULL) {
+            if(head->val != last->val)
+                return false;
+            head = head->next;
+            last = last->next;
+        }
+        
+        return true;
+    }
+};
