c655a1f0f9d9ac0fec4cb1754ed6b32c6a13f779
SeanCST <seancst.x@gmail.com>
2019-03-08 16:33:04 +0800

    21、83

--- LeetCode/83_删除排序链表中的重复元素.cpp
+++ LeetCode/83_删除排序链表中的重复元素.cpp
@@ -27,3 +27,14 @@ public:
         return head;
     }
 };
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL || head->next == NULL) 
+            return head;
+
+        head->next = deleteDuplicates(head->next);
+        return head->val == head->next->val ? head->next : head;
+    }
+};
