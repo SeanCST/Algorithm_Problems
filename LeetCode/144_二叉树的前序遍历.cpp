e8a18be4c577aff02766a502fb19e8974b9b3548
SeanCST <seancst.x@gmail.com>
2019-03-27 19:21:34 +0800

    144

---
+++ LeetCode/144_二叉树的前序遍历.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if(root == NULL) return res;
+
+        stack<TreeNode *> s;
+        s.push(root);
+        while(!s.empty()) {
+            TreeNode *pNode = s.top();
+            s.pop();
+            if(pNode == NULL) continue;
+            res.push_back(pNode->val);
+
+            // 先右后左，保证左子树先遍历
+            if(pNode->right)
+                s.push(pNode->right);
+            if(pNode->left)
+                s.push(pNode->left);            
+        }
+        
+        return res;
+    }
+    
+};
