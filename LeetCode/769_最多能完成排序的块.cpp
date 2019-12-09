54082c407cf8fdd144740f4f92a5746495a38888
SeanCST <seancst.x@gmail.com>
2019-12-09 22:06:13 +0800

    769

---
+++ LeetCode/769_最多能完成排序的块.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int res = 0;
+        // int left = 0;
+        // int innerCount = 0;
+        int innerMax = 0;
+        for(int i = 0; i < arr.size(); i++) {
+            innerMax = max(innerMax, arr[i]);
+            if(innerMax == i) {
+                res++;
+            }
+            // innerCount++;
+            // if(innerMax <= i && innerCount == i - left + 1) { // 找到对应区间
+            //     res++;
+            //     left = i + 1;
+            //     innerCount = 0;
+            // }
+        }
+
+        return res;
+    }
+};
