9140b6fb25e984dbf4e52ebd21b8b3e98d972cc3
SeanCST <seancst.x@gmail.com>
2020-01-18 16:49:31 +0800

    279

--- LeetCode/279_完全平方数.cpp
+++ LeetCode/279_完全平方数.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int numSquares(int n) {
+        vector<int> dp(n + 1, 0);
+        for(int i = 1; i <= n; i++) {
+            dp[i] = i; // 最坏的情况就是全部由 1 组成 
+            for(int j = 1; i - j * j >= 0; j++) {
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+};
+
+class Solution {
+public:
+    int numSquares(int n) {
          queue<pair<int,int>> q;
         
         q.push(make_pair(n,0)); // n 是给定的正整数，0 是初始步数
