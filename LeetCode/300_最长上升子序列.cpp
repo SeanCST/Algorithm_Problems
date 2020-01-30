7fdf34eaef7185fc503b4589eee7ebffde3aa106
SeanCST <seancst.x@gmail.com>
2020-01-30 22:13:44 +0800

    300

--- LeetCode/300_最长上升子序列.cpp
+++ LeetCode/300_最长上升子序列.cpp
@@ -5,12 +5,12 @@ public:
         if(n == 0) {
             return 0;
         }
-        vector<int> dp(n, 0);
+        // vector<int> dp(n, 0);
         // 有效区为 ends[0...right]，无效区为 ends[right+1 ... N-1]
         // 对有效区上的位置 b，如果有 ends[b] == c，表示遍历到目前为止，
         // 在所有长度为 b+1 的递增序列中，最小的结尾数是 c
         vector<int> ends(n, 0);
-        dp[0] = 1;
+        // dp[0] = 1;
         ends[0] = nums[0];
         int right = 0;
         int l = 0, r = 0, m = 0;
@@ -28,8 +28,9 @@ public:
             }
             right = max(right, l);
             ends[l] = nums[i];
-            dp[i] = l + 1;
-            maxLen = max(maxLen, dp[i]);
+            // dp[i] = l + 1;
+            // maxLen = max(maxLen, dp[i]);
+            maxLen = max(maxLen, l + 1);
         }
 
         return maxLen;
