6b1bb125b4885fbd7c8647ac855e22893f30a43b
SeanCST <seancst.x@gmail.com>
2020-03-15 23:41:31 +0800

    300

--- LeetCode/300_最长上升子序列.cpp
+++ LeetCode/300_最长上升子序列.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 0);
+        int maxLen = 0;
+        for(int num : nums) {
+            int l = 0, r = maxLen;
+            while(l < r) {
+                int mid = l + (r - l) / 2;
+                if(dp[mid] < num) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            }
+            dp[l] = num;
+            if(l == maxLen) {
+                maxLen++;
+            }
+        }
+
+        return maxLen;
+    }
+};
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         if(n == 0) {
             return 0;
