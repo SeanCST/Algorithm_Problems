efe76de07ec05a4c50cfaa62c99197d083bea136
SeanCST <seancst.x@gmail.com>
2020-03-30 16:05:19 +0800

    51

--- LeetCode/051_N皇后.cpp
+++ LeetCode/051_N皇后.cpp
@@ -1,6 +1,50 @@
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
+        vector<vector<int>> res;
+        vector<int> cur(n, -1);
+
+        backtrace(res, cur, 0, n);
+
+        vector<vector<string>> ans;
+        string s(n, '.');
+        for(vector<int> r : res) {
+            vector<string> v(n, s);
+            for(int i = 0; i < n; i++) {
+                v[i][r[i]] = 'Q';
+            }
+            ans.push_back(v);
+        }
+
+        return ans;
+    }
+
+    void backtrace(vector<vector<int>>& res, vector<int>& cur, int k, int n) {
+        if(k == n) {
+            res.push_back(cur);
+            return;
+        }
+
+        for(int i = 0; i < n; i++) {
+            bool conflict = false;
+            for(int j = 0; j < k; j++) {
+                if(cur[j] == i || abs(cur[j] - i) == abs(j - k)) {
+                    conflict = true;
+                    break;
+                }
+            }
+            if(!conflict) {
+                cur[k] = i;
+                backtrace(res, cur, k + 1, n);
+            }
+        }
+    }
+};
+
+
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
         vector<vector<int>> curSolutions;
         vector<int> cur(n, -1);
         solve(0, curSolutions, cur, n);
