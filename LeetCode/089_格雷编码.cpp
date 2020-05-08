1a8cd9828b05d8f618fd1ea86d824b2c3d54bb00
SeanCST <seancst.x@gmail.com>
2020-05-08 19:57:05 +0800

    089

--- LeetCode/089_格雷编码.cpp
+++ LeetCode/089_格雷编码.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
+        if(n == 0) {
+            return {0};
+        }
+        if(n == 1) {
+            return {0, 1};
+        }
+
+        vector<int> res;
+        vector<int> lastRes = grayCode(n - 1);
+
+        int base = 0;
+        for(int i = 0; i < lastRes.size(); i++) {
+            res.push_back(base | lastRes[i]);
+        }
+        base = 1 << n - 1;
+        for(int i = lastRes.size() - 1; i >= 0 ; i--) {
+            res.push_back(base | lastRes[i]);
+        }
+
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> grayCode(int n) {
         vector<int> res(1<<n, 0);
         for(int i = 0; i < 1<<n; i++) {
             res[i] = i ^ i>>1;
--- LeetCode/152_乘积最大子序列.cpp
+++ LeetCode/152_乘积最大子序列.cpp
@@ -20,3 +20,47 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n == 1) {
+            return nums[0];
+        }
+
+        vector<int> neg(n, 0);
+        vector<int> pos(n, 0);
+
+        int res = INT_MIN;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > 0) {
+                if(i == 0 || pos[i - 1] == 0) {
+                    pos[i] = nums[i];
+                } else {
+                    pos[i] = pos[i - 1] * nums[i];
+                }
+
+                if(i > 0 && neg[i - 1] != 0) {
+                    neg[i] = neg[i - 1] * nums[i];
+                }
+            } else if(nums[i] < 0) {
+                if(i == 0 || pos[i - 1] == 0) {
+                    neg[i] = nums[i];
+                } else {
+                    neg[i] = pos[i - 1] * nums[i];
+                }
+
+                if(i > 0 && neg[i - 1] != 0) {
+                    pos[i] = neg[i - 1] * nums[i];
+                }
+            } 
+
+            res = max(res, pos[i]);
+        }
+
+        return res;
+    }
+};
