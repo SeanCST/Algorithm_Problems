c79a01d961c7c3d572218edf4f1743e6543cce40
SeanCST <seancst.x@gmail.com>
2020-05-12 00:14:14 +0800

    785

--- LeetCode/785_判断二分图.cpp
+++ LeetCode/785_判断二分图.cpp
@@ -1,3 +1,37 @@
+
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> colors(n, -1);
+        
+        for(int i = 0; i < n; i++) {
+            if(colors[i] == -1 && !dfs(graph, colors, i, 0)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    bool dfs(vector<vector<int>>& graph, vector<int>& colors, int curNode, int curColor) {
+        if(colors[curNode] != -1) {
+            return colors[curNode] == curColor;
+        }
+
+        colors[curNode] = curColor;
+
+        for(int k : graph[curNode]) {
+            if(!dfs(graph, colors, k, 1 - curColor)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
+
+
 class Solution {
     int n;
 public:
