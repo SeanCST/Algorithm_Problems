06ed33439e7888da7bc004e31288baac185077ef
SeanCST <seancst.x@gmail.com>
2020-05-12 22:47:34 +0800

    044

--- LeetCode/044_通配符匹配.cpp
+++ LeetCode/044_通配符匹配.cpp
@@ -2,6 +2,37 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         int m = p.length(), n = s.length();
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+
+        // 初始化
+        dp[0][0] = true;
+        for(int i = 1; i <= m; i++) {
+            if(p[i - 1] == '*') {
+                dp[i][0] = true;
+            } else {
+                break;
+            }
+        }
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(p[i - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else if(p[i - 1] == '?' || p[i - 1] == s[j - 1]) { 
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = p.length(), n = s.length();
         if(m == 0 || n == 0) {
             return true;
         }
