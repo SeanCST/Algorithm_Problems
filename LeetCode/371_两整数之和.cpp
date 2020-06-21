f67fe10524e30348293481d56edc0cf2f49a0c15
SeanCST <seancst.x@gmail.com>
2020-06-21 19:56:11 +0800

    371

---
+++ LeetCode/371_两整数之和.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(b) {
+            int carry = a & b;
+            a = a ^ b;
+            b = (unsigned int)carry << 1;
+        }
+
+        return a;
+    }
+};
