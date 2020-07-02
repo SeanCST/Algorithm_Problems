9e405eb044aa206195e63b10ff90866112ea58d0
SeanCST <seancst.x@gmail.com>
2020-07-02 22:57:33 +0800

    151

--- LeetCode/151_翻转字符串里的单词.cpp
+++ LeetCode/151_翻转字符串里的单词.cpp
@@ -1,3 +1,53 @@
+// C
+
+void reverse(char *s, int curLen) {
+    int left = 0, right = curLen - 1;
+    while(left < right) {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+}
+
+char * reverseWords(char * s){
+    int i = 0;
+    for(int j = 0; s[j] != '\0'; j++) {
+        if((s[j] != ' ') || (i > 0 && s[j] == ' ' && s[j - 1] != ' ')) {
+            s[i++] = s[j];
+        }
+    }
+    int len;
+    if(i >= 1 && s[i - 1] == ' ') {
+        s[i - 1] = '\0';
+        len = i - 1;
+    } else {
+        s[i] = '\0';
+        len = i;
+    }
+
+    reverse(s, len);
+
+    int curLeft = 0, curLen = 0;
+    char *cur = s;
+    for(i = 0; i < len; i++) {
+        if(i + 1 == len || s[i + 1] == ' ') {
+            curLen = i - curLeft + 1;
+            reverse(cur, curLen);
+            curLeft = i + 2;
+            if(i + 1 < len) {
+                cur += curLen + 1;
+            }
+        }
+    }
+
+    return s;
+}
+
+
+// CPP
+
 class Solution {
 public:
     string reverseWords(string s) {
