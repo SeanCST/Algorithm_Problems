a5caf8cabd4ec8f79846d6218d428ea5608bf524
SeanCST <seancst.x@gmail.com>
2020-10-19 00:41:45 +0800

    450

--- LeetCode/450_删除二叉搜索树中的节点.cpp
+++ LeetCode/450_删除二叉搜索树中的节点.cpp
@@ -9,6 +9,39 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == NULL) {
+            return NULL;
+        } else if(root->val > key) {
+            root->left = deleteNode(root->left, key);
+        } else if (root->val < key) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            if(root->left == NULL && root->right == NULL) {
+                return NULL;
+            } else if(root->left != NULL && root->right == NULL) {
+                return root->left;
+            } else if(root->left == NULL && root->right != NULL) {
+                return root->right;
+            } else {
+                TreeNode *temp = root->right;
+                while(temp->left != NULL) {
+                    temp = temp->left;
+                }
+                root->val = temp->val;
+                root->right = deleteNode(root->right, root->val);
+            }
+        }
+
+        return root;
+    }
+};
+
+
+
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
