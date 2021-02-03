ba2b6e09494418b7785e3d4023d1077c880a0825
SeanCST <seancst.x@gmail.com>
2021-02-03 16:38:23 +0800

    448

---
+++ LeetCode/448_找到所有数组中消失的数字.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            nums[Math.abs(nums[i]) - 1] = - Math.abs(nums[Math.abs(nums[i]) - 1]);
+        }
+
+        for(Integer i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) {
+                res.add(i + 1);
+            }
+        }
+
+        return res;
+    }
+}
