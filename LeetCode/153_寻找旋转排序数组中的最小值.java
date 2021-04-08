f07ed0baa53dc84c6c848858340a076bc5b78c30
SeanCST <seancst.x@gmail.com>
2021-04-08 12:37:09 +0800

    153

---
+++ LeetCode/153_寻找旋转排序数组中的最小值.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int len = nums.length;
+        if(len == 1) {
+            return nums[0];
+        }
+        int l = 0, r = len - 1;
+        if(nums[l] < nums[r]) { // 等于没旋转
+            return nums[l];
+        }
+
+        // 真实旋转了，必存在 nums[i - 1] > nums[i]，其中 nums[i] 为最小值
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(nums[mid] > nums[mid + 1]) { // 找到最小值
+                return nums[mid + 1];
+            } else if(nums[l] < nums[mid]) { // 左边有序，最小值在右边
+                l = mid + 1;
+            } else { // 右边有序，最小值在左边（包含 mid）
+                r = mid;
+            }
+        }
+
+        return Math.min(nums[l], nums[r]);
+    }
+}
