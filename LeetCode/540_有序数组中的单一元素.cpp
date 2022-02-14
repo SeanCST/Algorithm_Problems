867fa1f3823d28acae64023abd2ad2bd79589feb
SeanCST <seancst.x@gmail.com>
2022-02-14 15:06:29 +0800

    540 java

--- LeetCode/540_有序数组中的单一元素.cpp
+++ LeetCode/540_有序数组中的单一元素.cpp
@@ -1,4 +1,30 @@
 class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+
+        int l = 0, r = n - 1;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(mid % 2 == 0) { 
+                if(nums[mid] != nums[mid + 1]) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
+            } else { 
+                if(nums[mid] == nums[mid - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            }
+        }
+
+        return nums[l];
+    }
+}
+
+class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int l = 0, h = nums.size() - 1;
