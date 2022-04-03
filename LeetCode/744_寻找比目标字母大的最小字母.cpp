39cb0981d0c92a97b78e45954880c8eaad8393e0
SeanCST <seancst.x@gmail.com>
2022-04-03 23:35:51 +0800

    744

--- LeetCode/744_寻找比目标字母大的最小字母.cpp
+++ LeetCode/744_寻找比目标字母大的最小字母.cpp
@@ -1,4 +1,24 @@
 class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int len = letters.length;
+        int l = 0, r = len - 1;
+
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(mid < len - 1 && letters[mid] <= target && letters[mid + 1] > target) {
+                return letters[mid + 1];
+            } else if (letters[mid] > target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return letters[0];
+    }
+}
+
+class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
         int l = 0, h = letters.size() - 1, mid;
