fb3d0a12c190b8d025702b47693b44ec3f3a56af
SeanCST <seancst.x@gmail.com>
2022-04-05 14:20:07 +0800

    762

---
+++ LeetCode/762_PrimeNumberOfSetBitsInBinaryRepresentation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        int res = 0;
+
+        for(int i = left; i <= right; i++) {
+            if(isPrime(Integer.bitCount(i))) {
+                res++;
+            }
+        }
+
+        return res;
+    }
+
+    private boolean isPrime(int n) {
+        if(n < 2) {
+            return false;
+        }
+
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
