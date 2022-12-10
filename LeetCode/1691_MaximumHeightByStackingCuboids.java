d73d4d0178328943502c7a646fa13b1d1e6ebb2c
SeanCST <seancst.x@gmail.com>
2022-12-10 14:01:54 +0800

    1691

---
+++ LeetCode/1691_MaximumHeightByStackingCuboids.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxHeight(int[][] cuboids) {
+        int n = cuboids.length;
+        for(int[] cuboid : cuboids) {
+            Arrays.sort(cuboid);
+        }
+        Arrays.sort(cuboids, (a, b) -> ((a[0] + a[1] + a[2]) - (b[0] + b[1] + b[2])));
+        int res = 0;
+        int[] dp = new int[n];
+        for(int i = 0; i < n; i++) {
+            dp[i] = cuboids[i][2];
+            for(int j = 0; j < i; j++) {
+                if(cuboids[j][0] <= cuboids[i][0] &&
+                    cuboids[j][1] <= cuboids[i][1] &&
+                    cuboids[j][2] <= cuboids[i][2]) {
+                        dp[i] = Math.max(dp[i], dp[j] + cuboids[i][2]);
+                    }
+            }
+            res = Math.max(res, dp[i]);
+        }
+
+        return res;
+    }
+}
