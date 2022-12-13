569f140787118f53104d51e6ad63753d27f168cb
SeanCST <seancst.x@gmail.com>
2022-12-13 22:34:24 +0800

    1832

---
+++ LeetCode/1832_CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        Set<Character> set = new HashSet<>();
+        for (char c : sentence.toCharArray()) {
+            if(c >= 'a' && c <= 'z') {
+                set.add(c);
+            }
+        }
+
+        return set.size() == 26;
+    }
+}
