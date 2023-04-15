96cb95065fd8d4b5e46af68ea4205e9b0e3d1da4
SeanCST <seancst.x@gmail.com>
2023-04-15 18:54:40 +0800

    1042

---
+++ LeetCode/1042_FlowerPlantingWithNoAdjacent.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] gardenNoAdj(int n, int[][] paths) {
+        List<Integer>[] adj = new List[n];
+        for(int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
+        for(int[] path : paths) {
+            adj[path[0] - 1].add(path[1] - 1);            
+            adj[path[1] - 1].add(path[0] - 1);
+        }
+
+        int[] res = new int[n];
+        for(int i = 0; i < n; i++) {
+            boolean colored[] = new boolean[5];
+            for(int vertex : adj[i]) {
+                colored[res[vertex]] = true;
+            }
+            for(int j = 1; j <= 4; j++) {
+                if(!colored[j]) {
+                    res[i] = j;
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+}
