03f7de3e4717677967d8b11ed4d7f0285debdb61
SeanCST <seancst.x@gmail.com>
2023-07-17 22:13:49 +0800

    415

--- LeetCode/415_字符串相加.java
+++ LeetCode/415_字符串相加.java
@@ -17,6 +17,34 @@ class Solution {
     }
 }
 
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1;
+        StringBuilder builder = new StringBuilder();
+        int carry = 0;
+        while(i >= 0 || j >= 0) {
+            int sum = carry;
+            if(i >= 0) {
+                sum += num1.charAt(i) - '0';
+                i--;
+            }
+            if(j >= 0) {
+                sum += num2.charAt(j) - '0';
+                j--;
+            }
+            carry = sum >= 10 ? 1 : 0;
+            sum %= 10;
+            builder.append(sum);
+        }
+
+        if(carry > 0) {
+            builder.append(1);
+        }
+
+        return builder.reverse().toString();
+    }
+}
+
 // class Solution {
 //     public String addStrings(String num1, String num2) {
 //         String r_num1 = new StringBuilder(num1).reverse().toString();
