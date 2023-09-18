335eae82204eb3780064c3ebd841556fbeb6be5d
SeanCST <seancst.x@gmail.com>
2023-09-18 23:52:58 +0800

    337

--- LeetCode/337_打家劫舍（三）.cpp
+++ LeetCode/337_打家劫舍（三）.cpp
@@ -26,3 +26,46 @@ public:
         return max(rob_money, noRob_money);
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private Map<TreeNode, Integer> map = new HashMap<TreeNode, Integer>();
+
+    public int rob(TreeNode root) {
+
+        if(root == null) {
+            return 0;
+        }
+
+        if(map.containsKey(root)) {
+            return map.get(root);
+        }
+
+        int res = root.val;
+        if(root.left != null) {
+            res += rob(root.left.left) + rob(root.left.right);
+        }
+        if(root.right != null) {
+            res += rob(root.right.left) + rob(root.right.right);
+        }
+
+        res = Math.max(res, rob(root.left) + rob(root.right));
+        map.put(root, res);
+        return res;
+    }
+}
