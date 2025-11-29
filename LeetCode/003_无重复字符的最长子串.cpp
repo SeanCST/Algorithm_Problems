774a457594acf1bb8fcf4ced1a3e8005be8dc083
SeanCST <seancst.x@gmail.com>
2025-11-29 15:01:25 +0800

    3

--- LeetCode/003_无重复字符的最长子串.cpp
+++ LeetCode/003_无重复字符的最长子串.cpp
@@ -1,4 +1,23 @@
 class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        int l = 0, res = 0;
+        for (int r = 0; r < s.length(); r++) {
+            Character c = s.charAt(r);
+            if (map.containsKey(c)) {
+                l = Math.max(l, map.get(c));
+            }
+
+            res = Math.max(res, r - l + 1);
+            map.put(c, r + 1);
+        }
+
+        return res;
+    }
+}
+
+class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int res = 0;
