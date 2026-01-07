b2541dc5b6781c3e92c535dc33899678a1314765
SeanCST <seancst.x@gmail.com>
2026-01-07 17:32:02 +0800

    031

---
+++ LeetCode/031_NextPermutation.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int len = nums.length;
+
+        int i = len - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = len - 1;
+            while (nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1, len - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            swap(nums, left++, right--);
+        }
+    }
+}
