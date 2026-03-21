246223e863e117eaded091a0a108d0fc712284bf
SeanCST <seancst.x@gmail.com>
2026-03-21 17:42:24 +0800

    154

--- LeetCode/154_FindMinimumInRotatedSortedArrayII.java
+++ LeetCode/154_FindMinimumInRotatedSortedArrayII.java
@@ -1,24 +1,49 @@
 class Solution {
     public int findMin(int[] nums) {
-        int l = 0, r = nums.length - 1;
+        int n = nums.length;
+
+        int l = 0, r = n - 1;
 
         while (l < r) {
             int mid = l + (r - l) / 2;
-            if (nums[mid] > nums[r]) {
-                l = mid + 1;
-            } else if (nums[mid] < nums[r]) {
+            if (nums[mid] < nums[r]) { // mid...r in order
                 r = mid;
+            } else if (nums[mid] > nums[r]) { // l...mid in order
+                l = mid + 1;
             } else {
-                int x = l;
-                for (int k = l + 1; k < r; k++) {
-                    if (nums[k] < nums[x]) {
-                        x = k;
-                    }
+                int i = l;
+                while (i < r && nums[i] >= nums[r]) {
+                    i++;
                 }
-                return nums[x];
+                return nums[i];
             }
         }
 
         return nums[l];
     }
 }
+
+// class Solution {
+//     public int findMin(int[] nums) {
+//         int l = 0, r = nums.length - 1;
+
+//         while (l < r) {
+//             int mid = l + (r - l) / 2;
+//             if (nums[mid] > nums[r]) {
+//                 l = mid + 1;
+//             } else if (nums[mid] < nums[r]) {
+//                 r = mid;
+//             } else {
+//                 int x = l;
+//                 for (int k = l + 1; k < r; k++) {
+//                     if (nums[k] < nums[x]) {
+//                         x = k;
+//                     }
+//                 }
+//                 return nums[x];
+//             }
+//         }
+
+//         return nums[l];
+//     }
+// }
--- LeetCode/155_最小栈.cpp
+++ LeetCode/155_最小栈.cpp
@@ -1,35 +1,3 @@
-class MinStack {
-    private Deque<Integer> stack;
-    private Deque<Integer> minStack;
-
-    public MinStack() {
-        stack = new ArrayDeque<>();
-        minStack = new ArrayDeque<>();
-    }
-    
-    public void push(int val) {
-        stack.push(val);
-        if (minStack.isEmpty() || minStack.peek() >= val) {
-            minStack.push(val);
-        }
-    }
-    
-    public void pop() {
-        int val = stack.pop();
-        if (minStack.peek() == val) {
-            minStack.pop();
-        }
-    }
-    
-    public int top() {
-        return stack.peek();
-    }
-    
-    public int getMin() {
-        return minStack.peek();
-    }
-}
-
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
