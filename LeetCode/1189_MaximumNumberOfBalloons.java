722bcc4ab4615fe6f4e0431bbb62f977bbaa1931
SeanCST <seancst.x@gmail.com>
2026-06-22 11:20:26 +0800

    1189

--- LeetCode/1189_MaximumNumberOfBalloons.java
+++ LeetCode/1189_MaximumNumberOfBalloons.java
@@ -1,20 +1,42 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
-
-        for(int i = 0; i < text.length(); i++) {
-            map.put(text.charAt(i), map.getOrDefault(text.charAt(i), 0) + 1);
+        int[] count = new int[26];
+        for (char c : text.toCharArray()) {
+            count[c - 'a']++;
         }
 
-        int b = map.getOrDefault('b', 0);
-        int a = map.getOrDefault('a', 0);
-        int l = map.getOrDefault('l', 0);
-        int o = map.getOrDefault('o', 0);
-        int n = map.getOrDefault('n', 0);
+        String target = "balloon";
+        Map<Character, Integer> targetMap = new HashMap<>();
+        for (char c : target.toCharArray()) {
+            targetMap.put(c, targetMap.getOrDefault(c, 0) + 1);
+        }
 
-        int count1 = Math.min(Math.min(b, a), n);
-        int count2 = Math.min(l / 2, o / 2);
+        int ans = Integer.MAX_VALUE;
+        for (Map.Entry<Character, Integer> entry : targetMap.entrySet()) {
+            ans = Math.min(ans, count[entry.getKey() - 'a'] / entry.getValue());
+        }
 
-        return Math.min(count1, count2);
+        return ans;
     }
 }
+
+// class Solution {
+//     public int maxNumberOfBalloons(String text) {
+//         Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+//         for(int i = 0; i < text.length(); i++) {
+//             map.put(text.charAt(i), map.getOrDefault(text.charAt(i), 0) + 1);
+//         }
+
+//         int b = map.getOrDefault('b', 0);
+//         int a = map.getOrDefault('a', 0);
+//         int l = map.getOrDefault('l', 0);
+//         int o = map.getOrDefault('o', 0);
+//         int n = map.getOrDefault('n', 0);
+
+//         int count1 = Math.min(Math.min(b, a), n);
+//         int count2 = Math.min(l / 2, o / 2);
+
+//         return Math.min(count1, count2);
+//     }
+// }
