0382b5ce9db8d08fe3f51b4512c6f8c44e3d2380
SeanCST <seancst.x@gmail.com>
2026-07-15 17:03:12 +0800

    3658

---
+++ LeetCode/3658_GCDOfOddAndEvenSums.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        int sumOdd = (1 + 2 * n - 1) * n / 2;
+        int sumEven = (2 + 2 * n) * n / 2;
+
+        return GCD(sumEven, sumOdd);
+    }
+
+    private int GCD(int a, int b) {
+        return a % b == 0 ? b : GCD(a, a % b);
+    }
+}
